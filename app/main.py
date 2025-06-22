de22460cb0170325182df84b1f170740c72acf3a
CalsonicSSS <calson.developer@gmail.com>
2025-06-22 14:17:06 -0400

    added comments

--- app/main.py
+++ app/main.py
@@ -52,6 +52,10 @@ app = FastAPI(
 )
 
 # Add CORS middleware
+# all 4 arugments are specific to the CORS middleware only, not generic within the add_middleware function
+# CORSMiddleware is one of the official premade middleware classes included in Starlette, which FastAPI is built on top of.
+# there are some other premade middleware classes in Starlette, such as GZipMiddleware, TrustedHostMiddleware, and SessionMiddleware.
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],  # Adjust this in production
